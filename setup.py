f28c251368d8976041bd97a53a4b1f2fc92b934f
Jeroen Tas <jeroentas@live.nl>
2021-07-23 15:34:56 +0200

    Preparation for publishing on PyPi

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+    name = 'pyeasyremote',
+    packages = ['pyeasyremote'],
+    version = '0.1',
+    license='MIT',
+    description = 'Control Easy Remote supporting DMX Lighting control software using python ',
+    author = 'Jeroen45',
+    author_email = 'hi@j45.nl',
+    url = 'https://github.com/Jeroen-45/pyeasyremote',
+    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords = ['Easy Remote'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+  ],
+)
